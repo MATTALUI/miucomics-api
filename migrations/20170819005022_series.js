0dfd0dcef068bfcd383eaba6c901ff1ad0ef9e32
Matthew Hummer <biggreyson@gmail.com>
2017-08-21 10:43:13 -0600

    update series table schema

--- migrations/20170819005022_series.js
+++ migrations/20170819005022_series.js
@@ -1,13 +1,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("series", function (table) {
   table.increments('id');
-  table.string('title').notNullable().defaultTo('');
-  table.integer('issue_number').notNullable().defaultTo('');
-  table.integer('series_number');
-  table.date('pub_date');
-  table.string('cover_image');
-  table.boolean('ebay').defaultTo(false);
-  table.boolean('shopify').defaultTo(false);
+  table.integer('volume');
 });
 
 };
