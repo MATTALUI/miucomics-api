c8223a8b000695ff2ba94812ecabe5416c736611
Matthew Hummer <biggreyson@gmail.com>
2017-09-05 14:40:26 -0600

    readd squareCall to updates

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ const comicsRoute = require('./routes/comics.js');
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 app.use(function (req, res, next) {
   res.removeHeader("X-Powered-By");
   res.set({'X-who-stole-the-cookies-from-the-cookie-jar': 'matt'});
@@ -40,36 +39,8 @@ app.get('/', function(req,res,next){
   }
 });
 app.get('/test',function(req,res,next){
-  let product = {
-  "product": {
-    "id": "1",
-    "title": "The Walking Dead Volume 1 Issue 1",
-    "images": [{"src":'https://s3.us-east-2.amazonaws.com/mixitupcomicimages/The-Walking-Dead-1-1.jpeg'}],
-    "variants": [
-      {
-        "option1": "Mint",
-        "price": "10.00"
-      },
-      {
-        "option1": "Very Good",
-        "price": "20.00"
-      }
-    ]
-  }
-};
-  let options = {
-    url: `https://miucomicsdevelopment.myshopify.com/admin/products.json`,
-    method: `POST`,
-    body: JSON.stringify(product),
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-      'Authorization': `Basic `
-    }
-  };
-  request(options,(error,response,body)=>{
-    res.send(body);
-  });
+  console.log(req);
+  res.send('tudo bom');
 });
 
 
