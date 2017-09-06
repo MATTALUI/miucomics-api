6fd34d9f6ed63fc2755d14d279ce9450533dfee5
Matthew Hummer <biggreyson@gmail.com>
2017-09-06 15:19:32 -0600

    filter shopify webhooks

--- routes/webhook.js
+++ routes/webhook.js
@@ -3,11 +3,21 @@ const router = express.Router();
 const queries = require('../helpers/queries.js');
 const squareCall = require('../helpers/square.js');
 const shopifyCall = require('../helpers/shopify.js');
+const fs = require('fs');
 
 
 router.use('/shopify', function(req,res,next){
-  console.log('shopify');
-  console.log(req.body.line_items);
+  let theStuffThatWeCareAbout = req.body.line_items.map((item)=>{
+    let info = {};
+    info.id = item.id;
+    info.variantId = item.variant_id;
+    info.quantity = item.quantity;
+    info.productId = item.product_id;
+    info.name = item.name;
+    return info;
+  });
+  // console.log(req.body.line_items);
+  console.log(theStuffThatWeCareAbout);
   res.sendStatus(200);
 });
 
