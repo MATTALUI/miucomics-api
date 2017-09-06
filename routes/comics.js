4ba35601eb26b3529b1921b2776de5526d548d2f
Matthew Hummer <biggreyson@gmail.com>
2017-09-06 09:59:14 -0600

    remove ids from stock seeds

--- routes/comics.js
+++ routes/comics.js
@@ -94,7 +94,7 @@ router.get('/stock/:id', function(req,res,next){
 
 router.post('/stock', function(req,res,next){
   queries.postNewStockInfo(req.body).then((newStockInfo)=>{
-    // squareCall.createSquareItemFromStocks(newStockInfo);
+    squareCall.createSquareItemFromStocks(newStockInfo);
     // shopifyCall.checkShopifyTrackingfromStockInfo(newStockInfo);
     res.send(newStockInfo)
   });
@@ -102,21 +102,21 @@ router.post('/stock', function(req,res,next){
 
 router.patch('/stock/:id',function(req,res,next){
   queries.updateStockPrice(req.params.id,req.body).then((stock)=>{
-    // squareCall.updatePrice(stock[0]);
+    squareCall.updatePrice(stock[0]);
     // shopifyCall.checkShopifyTrackingFromStockChange(stock[0]);
     res.sendStatus(200);
   });
 });
 router.put('/stock/:id', function(req,res,next){
   queries.increaseStockQuantity(req.params.id,req.body).then((stock)=>{
-    // squareCall.incrementStock(stock[0]);
+    squareCall.incrementStock(stock[0]);
     // shopifyCall.checkShopifyTrackingFromStockChange(stock[0]);
     res.sendStatus(200);
   });
 });
 router.delete('/stock/:id', function(req,res,next){
   queries.decreaseStockQuantity(req.params.id,req.body).then((stock)=>{
-    // squareCall.decrementStock(stock[0]);
+    squareCall.decrementStock(stock[0]);
     // shopifyCall.checkShopifyTrackingFromStockChange(stock[0]);
     res.sendStatus(200);
   });
